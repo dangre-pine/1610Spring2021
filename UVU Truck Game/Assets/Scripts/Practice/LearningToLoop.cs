0cbc5e3d3049963b2178f218d61e6f9c0391cd7e
Danielle Green <dangre.pine@gmail.com>
2021-01-30 13:31:54 -0700

    Tutorial and a Loop

---
+++ UVU Truck Game/Assets/Scripts/Practice/LearningToLoop.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningToLoop : MonoBehaviour
+{
+    int myInt = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("to start myInt = " + myInt);
+        myInt = countingLoop(myInt);
+        Debug.Log("myInt at the end = " + myInt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    //my first loopman
+    int countingLoop(int number)
+    {
+        while (number < 10)
+        {
+            number++;
+            Debug.Log("counter = " + number);
+            
+        }
+        
+        return number;
+    }
+}
---
+++ UVU Truck Game/Assets/Scripts/Practice/VariablesAndFunctions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesAndFunctions : MonoBehaviour
+{
+    private int myInt = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log("Int Print " + myInt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    // My own function called multiplybytwo
+    int MultiplyByTwo(int number)
+    {
+        int ret;
+        ret = number * 2;
+        return ret;
+    }
+}
