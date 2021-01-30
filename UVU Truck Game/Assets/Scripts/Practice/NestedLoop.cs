c8d179aec8a0adf443635e2f8d2606b7208de8d5
Danielle Green <dangre.pine@gmail.com>
2021-01-30 13:57:48 -0700

    More Loops

---
+++ UVU Truck Game/Assets/Scripts/Practice/NestedLoop.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NestedLoop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            Debug.Log("i = " + i);
+            for (int x = 1; x <= 10; x++)
+            {
+                Debug.Log("x = " + x);
+            
+            } 
+        } 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
