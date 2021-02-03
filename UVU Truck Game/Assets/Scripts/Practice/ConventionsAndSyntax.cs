75332583eeafac493f7ec38278f8866dc473bb84
Danielle Green <dangre.pine@gmail.com>
2021-02-02 23:11:43 -0700

    Syntax, Awake/Start, Update

---
+++ UVU Truck Game/Assets/Scripts/Practice/ConventionsAndSyntax.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConventionsAndSyntax : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(transform.position.x);
+
+        if (transform.position.y <= 5f)
+        {
+            // basically "im about to hit the ground!"
+            Debug.Log("I'm gunna munch, I'm gunna crunch!");
+        } 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
