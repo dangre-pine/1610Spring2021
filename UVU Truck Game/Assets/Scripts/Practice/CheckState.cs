a3b16dbdc277d01412074c87565f647cf6f8723f
Danielle Green <dangre.pine@gmail.com>
2021-02-03 23:05:22 -0700

    Check State

---
+++ UVU Truck Game/Assets/Scripts/Practice/CheckState.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckState : MonoBehaviour
+{
+    public GameObject myObject;
+    
+    // I'm going to go over this again, it's a bit confusing
+    void Start()
+    {
+        Debug.Log("Active Self: " + myObject.activeSelf);
+        Debug.Log("Activate in Hierarchy" + myObject.activeInHierarchy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
