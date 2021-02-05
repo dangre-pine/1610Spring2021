54728e33bd7c205fec764a0d2882093253527695
Danielle Green <dangre.pine@gmail.com>
2021-02-04 23:55:05 -0700

    Transform Functions

---
+++ UVU Truck Game/Assets/Scripts/Practice/TransformFunctions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+// not sure where to put this one but ooook
+    public float moveSpeed = 10f;
+
+    public float turnSpeed = 50f;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+        
+        if(Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+        
+        if(Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+    }
+}
