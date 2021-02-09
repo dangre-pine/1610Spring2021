8c58d29301e1618eecb153c5b95f624c089d1d85
Danielle Green <dangre.pine@gmail.com>
2021-02-08 23:13:27 -0700

    Ball Score / On Trigger

---
+++ UVU Truck Game/Assets/Scripts/BallScore.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScore : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private int score = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ((collision.gameObject.tag == "Guy") || (collision.gameObject.tag == "Head"))
+        {
+            //Debug.Log("trigger");
+            score++;
+            Debug.Log("the score is: " + score);
+           // print("the score is: " + score);
+            
+        }
+    }
+}
