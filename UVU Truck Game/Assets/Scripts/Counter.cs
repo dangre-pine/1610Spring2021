579532335fbf1cea1eac395e3b61b0cb04d94bdd
Danielle Green <dangre.pine@gmail.com>
2021-02-09 20:31:00 -0700

    Credit Counter

---
+++ UVU Truck Game/Assets/Scripts/Counter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+    public int CreditValue = 3;
+    public int numberCount;
+    public Text counterText;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D()
+    {
+        numberCount += CreditValue;
+        counterText.text = numberCount.ToString();
+       // gameObject.SetActive(false);
+    }
+}
