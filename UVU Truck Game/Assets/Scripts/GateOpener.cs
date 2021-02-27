2d4924659e2a76a114342c3fd2aafda20d1c98c3
Danielle Green <dangre.pine@gmail.com>
2021-02-26 18:44:31 -0700

    Gate Keeping

---
+++ UVU Truck Game/Assets/Scripts/GateOpener.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateOpener : MonoBehaviour
+{
+    public BoolData keyObj;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (keyObj.value)
+        {
+            gameObject.SetActive(false);
+        }
+
+    }
+}
