e6236ee1bd887f6c519f6d75fc9c2ee26f5db5aa
Danielle Green <dangre.pine@gmail.com>
2021-03-23 23:59:51 -0600

    Fire Bullet

---
+++ UVU Truck Game/Assets/Scripts/Sub/FireBullets.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBullets : MonoBehaviour
+{
+    public Transform firePosition;
+    public GameObject projectile;
+    public float fireForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fireForce = 20f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("space"))
+        {
+            GameObject bullet1 = Instantiate(projectile, firePosition.position, firePosition.rotation);
+            bullet1.GetComponent(Rigidbody2D > ().AddForce(firePosition.up * fireForce, ForceMode2D.Impulse);
+        }
+    }
+}
