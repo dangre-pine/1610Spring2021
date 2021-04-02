f31c9a74fb67d4818e8e0d222bebd76aa201de5a
Danielle Green <dangre.pine@gmail.com>
2021-04-01 23:56:14 -0600

    Challenge 1

---
+++ Create with Code/Prototype 1/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    public GameObject propeller;
+    public float speed = 5.0f;
+    public float rotationSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.left * rotationSpeed * Time.deltaTime);
+    }
+}
